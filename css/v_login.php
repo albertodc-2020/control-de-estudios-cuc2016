9f4b65c2dabdf060629692c1c4b95e99adce7bd5
albertodc <alberto.c@zoho.com>
2020-07-30 21:01:54 -0400

    Agregando carpeta con archivos .css

---
+++ css/v_login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="../css/estilologin.css"/>
+</head>
+<body>
+<div id="general"></div>
+<div id="login"	></div>
+	
+</body>
+</html>
